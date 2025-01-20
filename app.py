6f86089b2b5a5300d3c05f8b2ae455f6c74ca44f
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-01-20 12:14:06 +0000

    m

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import tempfile
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024  # 16MB max file size
 
 # Create uploads folder if it doesn't exist
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
